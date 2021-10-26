ce77525cc1a4178ab7551e153d292d91f91182c0
puneetjain-iclp <puneet.jain@iclployalty.com>
2021-10-26 16:17:55 +0100

    Update the setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
-from pip.req import parse_requirements
 
 
 try:
-    from setuptools import setup
+    from setuptools import setup,find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup,find_packages
 
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+    install_reqs = parse_requirements("requirements.txt",session = 'hack')
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    install_reqs = parse_requirements("requirements.txt")
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
 
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='tcg_secrets',
+    name='tcgsecrets',
     version='1.0.0',   # needs to be updated before every version /tag release
     description='Internal Collinson ',
     author='Puneet Jain',
     author_email='puneet.jain@collinsongroup.com',
     license='http://www.apache.org/licenses/LICENSE-2.0',
-    package_dir = 'core',
-
+    # package_dir = 'core',
+    packages = find_packages(exclude=['test']),
     long_description=open('README.md').read(),
     classifiers =[
         'Development Status :: 4 - Beta',
